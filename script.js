926c5ab83b6711011fcabd2174137f279348e6e1
ev8 <ev8@njit.edu>
2013-02-09 13:19:48 -0500

    variables, strings, comments and whitespace

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 alert("hello,world");
+
 //input
 var name = prompt("what is your name?");
 alert("hello, " + name);
+
 // variables, weak typed 
 var a;
 a = 5;
@@ -10,3 +12,28 @@ a = "five";
 a = 123.4535;
 a = -500;
 alert(a);
+
+// characters and strings
+var phrase =" he said \"escape\" the quotes";
+var phrase2 = "this is a siimple phrase."
+alert(phrase2.length);
+//concat strings
+alert(phrase+phrase2);
+// operators
+var a = 10;
+var b = 7;
+var result = a+b;
+prompt(result);
+result = (5+5)*10;
+prompt(result);
+
+result = result + 10;
+result += 10;
+result ++;
+
+// properly using white space
+//one statement one line
+
+
+
+
