79d153870c96099c63bdfc8db94173a50d465939
ev8 <ev8@njit.edu>
2013-02-09 13:00:05 -0500

    soft typed variable

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ alert("hello,world");
 //input
 var name = prompt("what is your name?");
 alert("hello, " + name);
+// variables, weak typed 
+var a;
+a = 5;
+a = 10000;
+a = "five";
+a = 123.4535;
+a = -500;
+alert(a);
